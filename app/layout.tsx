3462723295f51104000c996619bcff90d737a089
ramigou <dasul1021@gmail.com>
2024-01-30 00:21:25 +0900

    feat: set favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  icons: 'favicon.ico',
 };
 
 export default function RootLayout({
